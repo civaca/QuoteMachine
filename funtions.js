fa780d2f690cdd03947eed6974465f6e7692bdd4
civaca <gemelosvs@gmail.com>
2017-08-23 22:59:55 -0500

    First PAges

---
+++ funtions.js
@@ -0,0 +1,20 @@
+$('#but').on('click', function(e) {
+    e.preventDefault();
+    $.ajax( {
+      url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
+      success: function(data) {
+        var post = data.shift(); // The data is an array of posts. Grab the first one.
+        $('#title').text(post.title);
+        $('#text').html(post.content);
+
+        // If the Source is available, use it. Otherwise hide it.
+        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
+          $('#quote-source').html('Source:' + post.custom_meta.Source);
+        } else {
+          $('#quote-source').text('');
+        }
+      },
+      cache: false
+    });
+  });
+
